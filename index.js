fef781b23524942441b51c572448cabebf2633b8
rohan <rohanrajgautam@gmail.com>
2021-01-31 20:49:19 +0545

    added node json db

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require('express');
+const JsonDB = require('node-json-db').JsonDB;
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+const speakeasy = require('speakeasy')
+const uuid = require('uuid')
 
 const app = express()
 
+const db = new JsonDB(new Config("myDataBase", true, false, '/'));
+
 app.get('/api', (req, res) => {
-  res.json({ message: 'WElCOME'})
+  res.json({ message: 'Welcome to the two-factor-authentication example using speakeasy.'})
 })
 
 const PORT = process.env.PORT || 5000;
