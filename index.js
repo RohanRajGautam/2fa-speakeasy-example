7f8e7fd1498b54a0985af6a1e3ec0fd2170382ab
rohan <rohanrajgautam@gmail.com>
2021-02-01 10:30:33 +0545

    added validate token route

--- index.js
+++ index.js
@@ -59,6 +59,32 @@ app.post('/api/verify', (req, res) => {
   };
 })
 
+// Validate token
+app.post('/api/validate', (req, res) => {
+  const { userId, token } = req.body;
+  try {
+    // Retrieve user from db
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+    const { base32: secret } = user.secret;
+    // Returns true if the token matches
+    const tokenValidates = speakeasy.totp.verify({
+      secret,
+      encoding: 'base32',
+      token,
+      window: 1
+    });
+    if (tokenValidates) {
+      res.json({ validated: true });
+    } else {
+      res.json({ validated: false });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error retrieving user'})
+  };
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>  {
