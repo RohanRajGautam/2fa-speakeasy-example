dc15477c1bd9c3ccca003ee94cc31f8ee0321da2
rohan <rohanrajgautam@gmail.com>
2021-02-01 09:14:18 +0545

    added register user route

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Welcome to the two-factor-authentication example using speakeasy.'})
 })
 
+// Register user and create temp secret
+app.post('/api/register', (req, res) => {
+  const id = uuid.v4();
+  try {
+    const path = `/user/${id}`
+    //create temporary secret until verified
+    const temp_secret = speakeasy.generateSecret();
+    //create user in the db
+    db.push(path, { id, temp_secret });
+    //send user id and base32 key to user
+    res.json({ id, secret: temp_secret.base32 })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error generating secret key'})
+  }
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>  {
